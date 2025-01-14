00efadbaf44c1f87fd48670b6d27af558f535f77
Robby301 <roblego2001@comcast.net>
2025-01-13 21:49:44 -0500

    Added mouse sprite with running animation

--- scripts/player.gd
+++ scripts/player.gd
@@ -7,6 +7,7 @@ const ROTATION_SPEED = 20
 
 var character_direction : Vector2
 
+@onready var animated_sprite = $AnimatedSprite2D
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
@@ -34,5 +35,11 @@ func _physics_process(delta: float) -> void:
 		#$".".rotation = atan2(velocity.x, -velocity.y)
 	else:
 		velocity = velocity.move_toward(Vector2.ZERO, SPEED)
+		
+	# Animations
+	if character_direction == Vector2.ZERO:
+		animated_sprite.play("idle")
+	else:
+		animated_sprite.play("run")
 
 	move_and_slide()
