e012bd0f65f3e62f16534e574518196ea30c3574
Danpantzz <danpantz@gmail.com>
2025-01-14 19:33:37 -0500

    fixed enemy movement

--- scripts/enemy.gd
+++ scripts/enemy.gd
@@ -1,9 +1,13 @@
 extends CharacterBody2D
 
-const SPEED = 200
+const SPEED = 2
+const min_distance = 50;
+
+var player_position
+var target_position
 
 @onready var animated_sprite: AnimatedSprite2D = $AnimatedSprite2D
-@onready var player: CharacterBody2D = get_parent().get_parent().get_node("Player")
+@onready var player = get_parent().get_parent().get_node("Player")
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -14,12 +18,8 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _physics_process(delta: float) -> void:
 	
-	
-	
-	# move towards player
-	if (is_instance_valid(player)):
-		var direction = (player.position - position).normalized()
-		look_at(player.position)
-		velocity = direction * SPEED
-	move_and_slide()
-	pass
+	player_position = player.global_position
+	target_position = (player_position - global_position).normalized()
+	if (global_position.distance_to(player_position) > min_distance):
+		move_and_collide(target_position * SPEED)
+	look_at(player_position)
